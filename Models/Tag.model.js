4dabc2cfabfedb814d8633efbaf997da9b62b5a9
Saki279 <saki-lovetdr.1029@docomo.ne.jp>
2024-11-24 02:16:52 +0900

    add files

--- Models/Tag.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");  // mongooseをインポート
-const Schema = mongoose.Schema;        // Schemaを取得
-
-const TagSchema = new Schema({
-  name: { type: String, required: true, unique: true },
-}, { timestamps: true });
-
-const Tag = mongoose.model("Tag", TagSchema);
-module.exports = Tag;
